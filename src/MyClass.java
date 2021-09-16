b00827161813d19fdc51636cb14fc57a420f4d0b
EVoloshkan <katerinka13001@gmail.com>
2021-09-16 22:15:54 +0300

    Update1

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,37 +1,36 @@
 import java.util.Scanner;
 
 public class MyClass {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Выберите, что переводить: 1 - масса, 2 - расстояние");
         Scanner scanner = new Scanner(System.in);
         byte a = scanner.nextByte();
-        if(a == 1){
+        if (a == 1) {
             System.out.println("Выберите единицу измерения: 1 - грамм, 2 - килограмм, 3 - фунт");
             byte b = scanner.nextByte();
             System.out.println("Введите колличество");
             double c = scanner.nextDouble();
-             switch (b){
-                 case 1:
-                     System.out.println(c + " грамм");
-                     System.out.println(c * 1000 + " килограмм");
-                     System.out.println(c * 453.59 + " фунтов");
-                     break;
-                 case 2:
-                     System.out.println(c / 1000 + " грамм");
-                     System.out.println(c + " килограмм");
-                     System.out.println(c / 1000 * 453.59 + " фунтов");
-                     break;
-                 case 3:
-                     System.out.println(c / 453.59 + " грамм");
-                     System.out.println(c * 1000 / 453.59 + " килограмм");
-                     System.out.println(c + " фунтов");
-                     break;
-                 default:
-                     System.out.println("Неизвестная единица измерения");
-                     break;
-             }
-        }
-        else if (a == 2){
+            switch (b) {
+                case 1:
+                    System.out.println(c + " грамм");
+                    System.out.println(c / 1000 + " килограмм");
+                    System.out.println(c / 453.59 + " фунтов");
+                    break;
+                case 2:
+                    System.out.println(c * 1000 + " грамм");
+                    System.out.println(c + " килограмм");
+                    System.out.println(c * 2.205 + " фунтов");
+                    break;
+                case 3:
+                    System.out.println(c * 453.59 + " грамм");
+                    System.out.println(c / 2.205 + " килограмм");
+                    System.out.println(c + " фунтов");
+                    break;
+                default:
+                    System.out.println("Неизвестная единица измерения");
+                    break;
+            }
+        } else if (a == 2) {
             System.out.println("Выберите единицу измерения: 1 - метр, 2 - миля, 3 - ярд, 4 - фут");
             byte b = scanner.nextByte();
             System.out.println("Введите колличество");
@@ -57,16 +56,15 @@ public class MyClass {
                     break;
                 case 4:
                     System.out.println(c * 0.305 + " метр");
-                    System.out.println(c * 0.305/1609.34 + " миля");
-                    System.out.println(c * 0.305/0.91 + " ярд");
+                    System.out.println(c * 0.305 / 1609.34 + " миля");
+                    System.out.println(c * 0.305 / 0.91 + " ярд");
                     System.out.println(c + " фут");
                     break;
                 default:
                     System.out.println("Неизвестная единица измерения");
                     break;
             }
-        }
-        else {
+        } else {
             System.out.println("Некорректные данные");
         }
     }
